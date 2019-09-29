5a11dc7e7fd6cf16977fe08641e7fae55c25e041
NikolaTesla17 <44534701+NikolaTesla17@users.noreply.github.com>
2019-09-29 06:46:58 -0700

    cleaned the code and added a few comments

--- Final PaddleBall Game/red.js
+++ Final PaddleBall Game/red.js
@@ -1,9 +1,5 @@
-window.onload= function() {
-  Particles.
-init
-({
-    selector: '.background',
-    connectParticles: true
+window.onload= function() {//when everything has been loaded
+  Particles.init({//start this
+    selector: '.background',//in the background have some settings
+    connectParticles: true})//connect the particals
 }
-    );
-};
