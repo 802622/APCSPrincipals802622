ab24fc19ebc6f8ac6add7131166b12b82f56ce71
Nayan Smuek <nayansmuek@Nayans-MacBook-Pro.local>
2019-10-07 07:44:21 -0700

    done

--- Final PaddleBall Game/red.js
+++ Final PaddleBall Game/red.js
@@ -2,4 +2,4 @@ window.onload= function() {//when everything has been loaded
   Particles.init({//start this
     selector: '.background',//in the background have some settings
     connectParticles: true})//connect the particals
-}
+}//this section of the code was written by vincentgarreau
