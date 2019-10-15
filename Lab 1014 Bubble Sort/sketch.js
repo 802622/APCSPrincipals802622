d1c95fa956413b98b2f9ca66c3343230b9274181
Nayan Smuek <nayansmuek@Nayans-MacBook-Pro.local>
2019-10-15 08:43:33 -0700

    finished with randomized array

--- Lab 1014 Bubble Sort/sketch.js
+++ Lab 1014 Bubble Sort/sketch.js
@@ -1,11 +1,23 @@
 //  Nayan Smuek
 function setup() {
-  var list = [3,6,10,11,12,8,2,9,5,1,4];//to be sorted
-  iteration = 0;//to count how long it takes to sort
-  console.log("original: " + list);//put list pre sorted
+  var list = [];//to be sorted
+
+   //code from http://stackoverflow.com/questions/962802#962890
+  for (var a=[],i=0;i<2500;++i) a[i]=i;
+  function shuffle(array) {
+    var tmp, current, top = array.length;
+    if(top) while(--top) {
+      current = Math.floor(Math.random() * (top + 1));
+      tmp = array[current];
+      array[current] = array[top];
+      array[top] = tmp;}
+   return array;}
+   list = shuffle(a);
+   //end code from http://stackoverflow.com/questions/962802#962890
+
+iteration = 0;//to count how long it takes to sort
 sorting = true;//you need to sort
 
-// sorting = false;//no more sorting needed however the rest of the statment still finishes unless the itterate backwards loop sets it back to true because stuff is still out of order
 while (sorting == true){//if you need to sort then
    for (let x=0;x<list.length-1;x++){//while there is things to sort iterate up
       if (list[x] > list[x+1]){//check if a number is to far down
@@ -19,13 +31,13 @@ while (sorting == true){//if you need to sort then
 
    for (let j=list.length-1;j > 0;j--){//iterate backwards
       if (list[j-1] > list[j]){//if number is to far up
-         sorting = true;//sorting still needed
+         sorting = true;//sortiang still needed
       }//this is needed so the sort dosn't end up doing a whole extra iteration sometimes
    }//stop iterating
    iteration++;//say you did one loop
-   console.log("iteration: " + iteration + "\n arrary: " + list);//print out list at iteration to show change
+   //console.log("iteration: " + iteration + "\n arrary: " + list);//print out list at iteration to show change
 }//if sorting isnt true
-console.log("done: " + list);//say finished and print array
+console.log("done: " + iteration);//say finished and print array
 }//end setup
 
 //  The draw function is called @ 30 fps
