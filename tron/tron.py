510742bc93551ef26751f7109ac57237c64b9817
Nayan Smuek <nayansmuek@Nayans-MacBook-Pro.local>
2019-11-21 16:15:30 -0800

    done

--- tron/tron.py
+++ tron/tron.py
@@ -1,10 +1,13 @@
 import pygame
 import time
 pygame.init()
+pygame.font.init()
+
 
 BLACK = (0, 0, 0)
 BLUE = (36, 207, 237)#this thing has 30% cpu usage, probably do something about that
-RED = (246, 221, 73)
+RED = (246, 221, 73)#currently working, base game music and boosting
+#todo add loading screen explaning rules    add sound effects for boost and collisions figure out and fix bike disapering when boosting
 
 playerOneL = pygame.image.load('blueL.jpg')
 playerOneR = pygame.image.load('blueR.jpg')
@@ -14,6 +17,13 @@ playerTwoL= pygame.image.load('orangeL.jpg')
 playerTwoD = pygame.image.load('orangeD.jpg')
 playerTwoR = pygame.image.load('orangeR.jpg')
 playerTwoU = pygame.image.load('orangeU.jpg')
+
+song = 'wilds.ogg'
+
+pygame.mixer.init()
+pygame.mixer.music.load(song)
+
+pygame.mixer.music.play(-1)
 
 class Player:
     def __init__(self, x, y, d, c):
@@ -28,35 +38,34 @@ class Player:
         self.rect = pygame.Rect(self.x - 1, self.y - 1, 2, 2) 
         pygame.draw.rect(screen, self.col, self.rect, 0)
         if self.col == RED:
-            if self.dir == (-2, 0):#Left
+            if self.dir == (-2*x, 0):#Left
                 screen.blit(playerOneL, (self.x-30, self.y-10))
-            if self.dir == (2, 0):#Right
+            if self.dir == (2*x, 0):#Right
                 screen.blit(playerOneR, (self.x-20, self.y-10))
-            if self.dir == (0, -2):#Up
+            if self.dir == (0, -2*x):#Up
                 screen.blit(playerOneU, (self.x-8, self.y-30))
-            if self.dir == (0, 2):#Down
+            if self.dir == (0, 2*x):#Down
                 screen.blit(playerOneD, (self.x-10, self.y-10))
 
-        if self.col == BLUE:
-            if self.dir == (-2, 0):#Left good
+        elif self.col == BLUE:
+            if self.dir == (-2*y, 0):#Left good
                 screen.blit(playerTwoL, (self.x-30, self.y-10))
-            if self.dir == (2, 0):#Right
+            if self.dir == (2*y, 0):#Right
                 screen.blit(playerTwoR, (self.x-10, self.y-10))
-            if self.dir == (0, -2):#Up
+            if self.dir == (0, -2*y):#Up
                 screen.blit(playerTwoU, (self.x-12, self.y-30))
-            if self.dir == (0, 2):#Down
+            if self.dir == (0, 2*y):#Down
                 screen.blit(playerTwoD, (self.x-10, self.y-10))
 
     def __update__(self):
             self.x += self.dir[0]
             self.y += self.dir[1]
 
-
 def new_game():
+
     new_p1 = Player(50, 600 / 2, (2, 0), RED)
     new_p2 = Player(600 - 50, 600 / 2, (-2, 0), BLUE)
     return new_p1, new_p2
-
 
 screen = pygame.display.set_mode((600, 600))
 
@@ -78,36 +87,77 @@ wall_rects = [pygame.Rect([0, 60, 15, 600]) , pygame.Rect([0, 60, 600, 15]),\
               pygame.Rect([600 - 15, 60, 15, 600]),\
               pygame.Rect([0, 600 - 15, 600, 15])]
 
-done = False
 new = False
+done = False
+
+x = 1
+y = 1
+
+
+def load_screen():
+    v = False
+    while v == False:
+        event = pygame.event.wait()
+
+        screen.fill(BLACK) 
+
+        font = pygame.font.Font('freesansbold.ttf', 32) 
+        text = font.render("Welcome To Tron", True, BLUE) 
+        textRect = text.get_rect() 
+        textRect.center = (300, 200) 
+        screen.blit(text, textRect) 
+
+        font = pygame.font.Font('freesansbold.ttf', 16) 
+        text = font.render("Press any key to continue", True, BLUE) 
+        textRect = text.get_rect() 
+        textRect.center = (300, 300) 
+        screen.blit(text, textRect) 
+        pygame.display.update()
+
+        if event.type == pygame.KEYDOWN:
+            v = True    
+
+load_screen()
 
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:  # close button pressed
             done = True
         elif event.type == pygame.KEYDOWN:
+
+            if event.key == pygame.K_LSHIFT:
+                x = 2
+                print("you could be boosting")
+            if event.key == pygame.K_RSHIFT:
+                y = 2
+                print("you could be boosting")
+            
           
             if event.key == pygame.K_w:
-                objects[0].dir = (0, -2)
+                objects[0].dir = (0, -2*x)
             elif event.key == pygame.K_s:
-                objects[0].dir = (0, 2)
+                objects[0].dir = (0, 2*x)
             elif event.key == pygame.K_a:
-                objects[0].dir = (-2, 0)
+                objects[0].dir = (-2*x, 0)
             elif event.key == pygame.K_d:
-                objects[0].dir = (2, 0)
+                objects[0].dir = (2*x, 0)
 
             if event.key == pygame.K_UP:
-                objects[1].dir = (0, -2)
+                objects[1].dir = (0, -2*y)
             elif event.key == pygame.K_DOWN:
-                objects[1].dir = (0, 2)
+                objects[1].dir = (0, 2*y)
             elif event.key == pygame.K_LEFT:
-                objects[1].dir = (-2, 0)
+                objects[1].dir = (-2*y, 0)
             elif event.key == pygame.K_RIGHT:
-                objects[1].dir = (2, 0)
+                objects[1].dir = (2*y, 0)
 
     screen.fill(BLACK) 
+    tim = pygame.time.get_ticks()
+    if tim%50 == 0:
+        x = 1
+        y = 1
 
-    for r in wall_rects: pygame.draw.rect(screen, (42, 42, 42), r, 0) 
+    for r in wall_rects: pygame.draw.rect(screen, (50, 50, 50), r, 0) 
 
     for o in objects:
 
@@ -124,9 +174,11 @@ while not done:
                 new_p1, new_p2 = new_game()
                 objects = [new_p1, new_p2]
                 path = [(p1.rect, '1'), (p2.rect, '2')]
-                break
         else: 
-            path.append((o.rect, '1')) if o.col == BLUE else path.append((o.rect, '2'))
+            if o.col == BLUE:
+                path.append((o.rect, '1'))
+            else:
+                path.append((o.rect, '2'))
 
         o.__draw__()
         o.__update__()
@@ -136,8 +188,8 @@ while not done:
             path = []
             new = False
             break
-        if r[1] == '1': pygame.draw.rect(screen, RED, r[0], 0)
-        else: pygame.draw.rect(screen, BLUE, r[0], 0)
+        if r[1] == '1': pygame.draw.rect(screen, RED, r[0], 1)
+        else: pygame.draw.rect(screen, BLUE, r[0], 1)
     
     font = pygame.font.Font('freesansbold.ttf', 32) 
     score1 = "{}".format(player_score[1])
